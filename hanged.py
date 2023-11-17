5893522846fb845b2d7e93a479c8040941522952
UltimeCookie777 <trove.jumpx@gmail.com>
2023-11-17 12:42:50 +0100

    changed language to english and some fixes

--- hanged.py
+++ hanged.py
@@ -1,10 +1,10 @@
 import os
-maxAttempts, attempts, mot = 8, 0, [c for c in input("Mot à trouver : ").lower()]
-guess = ["-"] * len(mot)
+attempts, mot = 0, [c for c in input("Word to find : ").lower()]
+guess, maxAttempts = ["-"] * len(mot), 9 + len(set(mot))
 os.system('cls')
 while not(guess == mot) and not(attempts == maxAttempts):
-    lettre, attempts = input("Lettre : ").lower(), attempts+1
+    letter, attempts = input("Letter : ").lower(), attempts+1
     for idx, val in enumerate(mot):
-        guess[idx] = lettre if lettre == val else guess[idx]
+        guess[idx] = letter if letter == val else guess[idx]
     print(guess)
-print("Tu es réussi, le mot était " + "".join(mot) if guess==mot else "Perdu ! Le mot était "+ "".join(mot))
+print("You won ! the word was " + "".join(mot) if guess==mot else "Lost ! The word was "+ "".join(mot))
